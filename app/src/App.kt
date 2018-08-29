8aff445ee55d6d441342738d0abc7d9a8aca142e
hazae41 <hazae41@gmail.com>
2018-08-30 00:27:29 +0200

    .

--- app/src/App.kt
+++ app/src/App.kt
@@ -1,22 +1,9 @@
 package fr.rhaz.ipfs.sweet
 
 import android.app.Application
-import fr.rhaz.ipfs.sweet.di.AppComponent
-import fr.rhaz.ipfs.sweet.di.AppModule
-import fr.rhaz.ipfs.sweet.di.DaggerAppComponent
 
 import org.ligi.tracedroid.TraceDroid
 
 class App : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        component = DaggerAppComponent.builder().appModule(AppModule()).build()
-        TraceDroid.init(this)
-    }
-
-    companion object {
-        private var component: AppComponent? = null
-        fun component() = component
-    }
+    override fun onCreate() = super.onCreate().also{TraceDroid.init(this)}
 }
