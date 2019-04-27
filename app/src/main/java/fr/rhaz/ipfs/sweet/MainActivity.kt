b70082595ca79afa1121223ef657e63904baade9
hazae41 <hazae41@gmail.com>
2019-04-27 13:11:06 +0200

    bump

---
+++ app/src/main/java/fr/rhaz/ipfs/sweet/MainActivity.kt
@@ -0,0 +1,13 @@
+package fr.rhaz.ipfs.sweet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
