1bd57686d8255e5441b3e7d1e58e844a64b01330
hazae41 <hazae41@gmail.com>
2019-04-27 23:12:57 +0200

    Readded automatic assets from @leophis code Added web activity

--- app/src/main/java/fr/rhaz/ipfs/sweet/WebActivity.kt
+++ app/src/main/java/fr/rhaz/ipfs/sweet/WebActivity.kt
@@ -2,14 +2,13 @@ package fr.rhaz.ipfs.sweet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_web.*
 
-class MainActivity : AppCompatActivity() {
+class WebActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_web)
 
     }
-
-
 }
