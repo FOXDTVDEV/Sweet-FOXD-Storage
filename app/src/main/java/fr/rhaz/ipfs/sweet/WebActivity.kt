0efecaedef0459cc67625a6859d103af33af628d
hazae41 <hazae41@gmail.com>
2019-05-08 23:03:16 +0200

    wip

--- app/src/main/java/fr/rhaz/ipfs/sweet/WebActivity.kt
+++ app/src/main/java/fr/rhaz/ipfs/sweet/WebActivity.kt
@@ -1,14 +1,23 @@
 package fr.rhaz.ipfs.sweet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_web.*
+
 
 class WebActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
-
+        startService<DaemonService>()
+        webview!!.apply {
+            settings.javaScriptEnabled = true
+            webChromeClient = WebChromeClient()
+            webViewClient = WebViewClient()
+            loadUrl("http://127.0.0.1:5001/webui")
+        }
     }
 }
